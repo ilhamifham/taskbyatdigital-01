81ba14f19618ac5b33e9f527975e81e2f0a3954e
Ilham Ifham <ilhaamifham@gmail.com>
2025-08-15 22:58:00 +0530

    Initial commit project created

---
+++ src/app/page.tsx
@@ -0,0 +1,27 @@
+import { Metadata } from "next";
+
+import { SliceZone } from "@prismicio/react";
+
+import { createClient } from "@/prismicio";
+import { components } from "@/slices";
+
+export default async function Home() {
+  const client = createClient();
+  const home = await client.getByUID("page", "home");
+
+  return <SliceZone slices={home.data.slices} components={components} />;
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient();
+  const home = await client.getByUID("page", "home");
+
+  return {
+    title: home.data.title,
+    description: home.data.meta_description,
+    openGraph: {
+      title: home.data.meta_title ?? undefined,
+      images: [{ url: home.data.meta_image.url ?? "" }],
+    },
+  };
+}
